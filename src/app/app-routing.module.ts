76ee3e8e3b1b111ab07a351538928fb709d852e0
ankitborude <ankitborude250@gmail.com>
2024-04-11 09:52:10 +0530

    Registratio route configured

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { StudentLoginComponent } from './home-page/student-login/student-login.component';
+import { AdminLoginComponent } from './home-page/admin-login/admin-login.component';
+import { StudentRegisterComponent } from './home-page/student-register/student-register.component';
+import { AdminRegisterComponent } from './home-page/admin-register/admin-register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/homepage', pathMatch: 'full' },
+  {path:'',component:AppComponent,children:[
+  {path:'homepage',component:HomePageComponent,children:[
+    { path: '', redirectTo: 'student-login', pathMatch: 'full' },
+    {path:'student-login',component:StudentLoginComponent},
+    {path:'admin-login',component:AdminLoginComponent},
+    {path:'student-register',component:StudentRegisterComponent},
+    {path:'admin-register',component:AdminRegisterComponent}
+  ]}
+]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

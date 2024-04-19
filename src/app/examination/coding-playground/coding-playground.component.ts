5f88c64e05cf8202ed6794f04c21f28494473db3
ankitborude <ankitborude250@gmail.com>
2024-04-19 20:44:23 +0530

    basic code editor is ready

--- src/app/examination/coding-playground/coding-playground.component.ts
+++ src/app/examination/coding-playground/coding-playground.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild, inject } from '@angular/core';
+import { python} from '@codemirror/lang-python';
+import {lineNumbers} from '@codemirror/gutter'
+import { EditorState, Extension } from '@codemirror/state';
+import { EditorView, keymap, highlightActiveLine } from '@codemirror/view';
+
+import {
+  syntaxHighlighting,
+  defaultHighlightStyle,
+} from '@codemirror/language';
+
+import {
+  oneDark,
+  oneDarkTheme,
+  oneDarkHighlightStyle,
+} from '@codemirror/theme-one-dark';
+import { basicSetup } from 'codemirror';
 
 @Component({
   selector: 'app-coding-playground',
   templateUrl: './coding-playground.component.html',
   styleUrl: './coding-playground.component.css'
 })
-export class CodingPlaygroundComponent {
+export class CodingPlaygroundComponent implements AfterViewInit {
+document:Document;
+@ViewChild('editor')
+editor:ElementRef
+constructor(){
 
+}
+  ngAfterViewInit(): void {
+    {
+      const myEditorElement = this.editor.nativeElement;
+      let Istate!: EditorState;
+      const MyExtension: Extension = [
+        basicSetup,
+        syntaxHighlighting(defaultHighlightStyle, { fallback: true }),
+        highlightActiveLine(),
+        python(),
+       
+      ];
+    
+      try {
+        Istate = EditorState.create({
+          extensions: [MyExtension],
+          doc:"Type Your Code here",
+          
+        });
+      } catch (e) {
+        console.error(e);
+      }
+  
+      const view = new EditorView({ state:Istate, parent: myEditorElement });
+    }
+}
 }

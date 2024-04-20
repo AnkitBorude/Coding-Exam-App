2dcbec7f07621943f32f954122e6d55ba5c1367f
ankitborude <ankitborude250@gmail.com>
2024-04-20 23:34:56 +0530

    Some route changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
@@ -6,6 +6,11 @@ import { StudentLoginComponent } from './home-page/student-login/student-login.c
 import { AdminLoginComponent } from './home-page/admin-login/admin-login.component';
 import { StudentRegisterComponent } from './home-page/student-register/student-register.component';
 import { AdminRegisterComponent } from './home-page/admin-register/admin-register.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { StudentDashboardComponent } from './student-dashboard/student-dashboard.component';
+import { ExaminationComponent } from './examination/examination.component';
+import { CreateExamComponent } from './admin-dashboard/create-exam/create-exam.component';
+import { CreateQuestionsComponent } from './admin-dashboard/create-exam/create-questions/create-questions.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/homepage', pathMatch: 'full' },
@@ -16,7 +21,15 @@ const routes: Routes = [
     {path:'admin-login',component:AdminLoginComponent},
     {path:'student-register',component:StudentRegisterComponent},
     {path:'admin-register',component:AdminRegisterComponent}
-  ]}
+  ]},
+  {path:'admin-dashboard',component:AdminDashboardComponent,children:[
+    { path: '', redirectTo: 'create-exam', pathMatch: 'full' },
+    { path: 'create-exam', component:CreateExamComponent,children:[
+      {path:'add-questions/:exam_id',component:CreateQuestionsComponent}
+    ]},
+  ]},
+  {path:'student-dashboard',component:StudentDashboardComponent},
+  {path:'examination',component:ExaminationComponent}
 ]}
 ];
 

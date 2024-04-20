540bc5edf6d4896e6f7e3e4342d1cb3cf7eeec9d
ankitborude <ankitborude250@gmail.com>
2024-04-20 22:54:28 +0530

    Admin Login Added

--- src/app/home-page/admin-login/admin-login.component.ts
+++ src/app/home-page/admin-login/admin-login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { homepageService } from '../home-page.service';
+import { AdminService } from '../../admin.service';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-login',
@@ -8,12 +11,39 @@ import { homepageService } from '../home-page.service';
 })
 export class AdminLoginComponent implements OnInit {
   homepages:homepageService;
-  constructor()
+  admin:AdminService;
+  loginForm:FormGroup;
+  alertMessage: string | null = null;
+  alertType: string | null = null;
+  constructor(private fb: FormBuilder, private http: HttpClient)
   {
     this.homepages=inject(homepageService);
+    this.admin=inject(AdminService);
+    this.loginForm = this.fb.group({
+      admin_user_name: ['', Validators.required],
+      admin_password: ['', Validators.required]
+    });
   }
   ngOnInit(): void
   {
     this.homepages.registrationActivated.next(false);
+  }
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const formData = this.loginForm.value;
+      this.http.post('http://localhost:3000/api/admin/login', formData)
+        .subscribe(
+          (response: any) => {
+            this.alertMessage = response.message || 'Login successful, thank you';
+            this.alertType = 'success';
+            console.log(response.adminId);
+            this.admin.setAdminId(response.adminId);
+          },
+          (error: any) => {
+            this.alertMessage = error.error.error;
+            this.alertType = 'danger';
+          }
+        );
+    }
   }
 }
